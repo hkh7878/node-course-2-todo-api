137318bc8e42072adb9ee34d56cc696444e72c34
Han Kwang Hoon <kwanghoon.han@gmail.com>
2017-07-27 11:31:22 +0900

    Setup app for heroku

--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
 var app = express();
+const port = process.env.PORT || 3000;
 
 // 미들웨어
 app.use(bodyParser.json());
@@ -55,8 +56,8 @@ app.get('/todos/:id', (req, res) => {
 });
 
 // 서버시작
-app.listen(3000, () => {
-  console.log('Started on port 3000');
+app.listen(port, () => {
+  console.log(`Started on port ${port}`);
 });
 
 module.exports = {app};
