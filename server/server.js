221702684dea49f6eaca2df7a22a4a4da134cc23
Han Kwang Hoon <kwanghoon.han@gmail.com>
2017-07-28 10:38:36 +0900

    Add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 // 모듈
-var express = require('express');
-var bodyParser = require('body-parser');
-var {ObjectID} = require('mongodb');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 // custom 모듈
 var {mongoose} = require('./db/mongoose');
@@ -68,6 +69,36 @@ app.delete('/todos/:id', (req, res) => {
     }
 
     res.status(200).send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
+
+app.patch('/todos/:id', (req, res) => {
+  // 업데이트하고자 하는 정보를 가져온다
+  var id = req.params.id;
+  var body = _.pick(req.body, ['text', 'completed']);
+
+  // 업데이트 데이터를 점검한다
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  // 업데이트 데이터를 생성한다
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  // 업데이트를 수행한다
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send({todo});
   }).catch((e) => {
     res.status(400).send();
   });
